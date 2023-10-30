6c19d7ee01b3cfad9fe6522a8cffd5f67b8af081
Rahul Vishwakarma <119070053+manzil-infinity180@users.noreply.github.com>
2023-10-30 19:33:16 +0530

    Update facbook.js

--- OAuth-Passport/facbook.js
+++ OAuth-Passport/facbook.js
@@ -1,47 +1,71 @@
-const passport = require("passport");
-const FacebookStrategy = require("passport-facebook");
-const port = 5000;
-const express = require('express');
-const session = require("express-session");
-const app = express();
+const passport = require('passport');
+const express=require("express");
+const session = require('express-session');
+const FacebookStrategy = require("passport-facebook").Strategy;
+const dotenv = require("dotenv");
+const app=express();
 
+dotenv.config({path:'./config.env'});
 
 passport.use(new FacebookStrategy({
-  clientId: 1662863714238839,
+  clientID: process.env.FACEBOOK_ID,
   clientSecret: process.env.FACEBOOK_SECRET,
   callbackURL: process.env.FACEBOOK_CALLBACK
 },
 (accessToken,refreshToken,profile,cb)=>{
+  const option={
+    accessToken,
+    refreshToken,
+    profile 
+  };
+  console.log(option);
   return cb(null,profile);
 }
-
 ));
 
 app.use(
   session({
-    resave: false,
-    saveUninitialized: true,
-    secret: process.env.FACBOOK_SECRET,
+    resave:false,
+    saveUninitialized:true,
+    secret:process.env.FACEBOOK_SECRET
   })
-);
+)
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/',(req,res)=>{
-  res.send("Login with Facebook - auth/facebook");
-})
-app.get('/login/facebook', passport.authenticate('facebook'));
 
-app.get('/login/facebook', passport.authenticate('facebook', {
-  scope: [ 'email', 'user_location' ]
-}));
+app.get('/',(req,res)=>{
+  res.send('Head over to /auth/facebook');
+
+});
+app.get('/auth/facebook',
+passport.authenticate('facebook',{
+  scope:['email']
+})
+);
+// app.get('/auth/facebook',passport.authenticate('facebook',{
+//   scope:['email']
+// }))
 
 app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login', failureMessage: true }),
-  function(req, res) {
-    res.redirect('/');
-  });
+passport.authenticate('facebook', { failureRedirect: '/login', failureMessage: true }),
+(req,res)=>{
+  res.redirect('/success');
+});
+app.get('/success',(res,req)=>{
+  res.send("successfully login!!");
+})
 
-  app.listen(port,()=>{
-    console.log("listening on port 5000");
-  })
+passport.serializeUser(function (user, cb) {
+  return cb(null, user);
+});
+passport.deserializeUser(function (obj, cb) {
+ return cb(null, obj);
+});
 
+app.get('/login',(req,res)=>{
+  res.send("Go to http://localhost:3000/auth/facebook for login ")
+});
+
+app.listen(3000,()=>{
+  console.log('Listening on port 3000');
+})
